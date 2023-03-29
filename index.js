cc0295654ae97f48737d3802e6428c9eb5652009
Syeda Maria <uroos12345@gmail.com>
2023-03-29 15:39:36 +0500

    atm working feature

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 import inquirer from "inquirer";
 let user = {
     name: "maria",
-    pin: 123,
+    pin: 1234,
     balance: 10000,
 };
 const resp = await inquirer.prompt([
@@ -12,39 +12,55 @@ const resp = await inquirer.prompt([
         type: "password",
     },
 ]);
+let continueTransaction = true;
 if (Number(resp.pin) !== user.pin) {
     console.log("password invalid");
 }
 else {
-    const resp = await inquirer.prompt([
-        {
-            name: "SelectedType",
-            message: "please select option",
-            type: "list",
-            choices: ["withdraw", "fastcash", "balanceinquiry"],
-        },
-        {
-            name: "amount",
-            message: "please select amount",
-            type: "list",
-            choices: ["1000", "2000", "5000", "10000"],
-            when(resp) {
-                return resp.SelectedType == "fastcash";
+    while (continueTransaction === true) {
+        const resp = await inquirer.prompt([
+            {
+                name: "SelectedType",
+                message: "please select option",
+                type: "list",
+                choices: ["withdraw", "fastcash", "balanceinquiry"],
             },
-        },
-        {
-            name: "amount",
-            message: "please enter amount",
-            when(resp) {
-                return resp.SelectedType == "withdraw";
+            {
+                name: "amount",
+                message: "please select amount",
+                type: "list",
+                choices: ["1000", "2000", "5000", "10000"],
+                when(resp) {
+                    return resp.SelectedType == "fastcash";
+                },
             },
-        },
-    ]);
-    if (resp.SelectedType == "balanceinquiry") {
-        console.log(`avaialable balance is: ${user.balance}`);
-    }
-    else {
-        user.balance = user.balance - resp.amount;
-        console.log(`your new balance is:${user.balance}`);
+            {
+                name: "amount",
+                message: "please enter amount",
+                when(resp) {
+                    return resp.SelectedType == "withdraw";
+                },
+            },
+        ]);
+        if (resp.SelectedType == "balanceinquiry") {
+            console.log(`avaialable balance is: ${user.balance}`);
+            const toRepeat = await inquirer.prompt([
+                {
+                    name: "repeat",
+                    type: "confirm",
+                    message: "do you want to repeat transaction?",
+                },
+            ]);
+            if (toRepeat.repeat === true)
+                continueTransaction = true;
+            else {
+                continueTransaction = false;
+            }
+        }
+        else {
+            user.balance = user.balance - resp.amount;
+            console.log(`your new balance is:${user.balance}`);
+            continueTransaction = false;
+        }
     }
 }
--- index.ts
+++ index.ts
@@ -19,7 +19,7 @@ type Usertype = {
 };
 let user: Usertype = {
   name: "maria",
-  pin: 123,
+  pin: 1234,
   balance: 10000,
 };
 
@@ -31,39 +31,55 @@ const resp = await inquirer.prompt([
   },
 ]);
 
+let continueTransaction: boolean = true;
+
 if (Number(resp.pin) !== user.pin) {
   console.log("password invalid");
 } else {
-  const resp = await inquirer.prompt([
-    {
-      name: "SelectedType",
-      message: "please select option",
-      type: "list",
-      choices: ["withdraw", "fastcash", "balanceinquiry"],
-    },
-
-    {
-      name: "amount",
-      message: "please select amount",
-      type: "list",
-      choices: ["1000", "2000", "5000", "10000"],
-      when(resp) {
-        return resp.SelectedType == "fastcash";
+  while (continueTransaction === true) {
+    const resp = await inquirer.prompt([
+      {
+        name: "SelectedType",
+        message: "please select option",
+        type: "list",
+        choices: ["withdraw", "fastcash", "balanceinquiry"],
       },
-    },
-    {
-      name: "amount",
-      message: "please enter amount",
-      when(resp) {
-        return resp.SelectedType == "withdraw";
-      },
-    },
-  ]);
-  if (resp.SelectedType == "balanceinquiry") {
-    console.log(`avaialable balance is: ${user.balance}`);
-  } else {
-    user.balance = user.balance - resp.amount;
 
-    console.log(`your new balance is:${user.balance}`);
+      {
+        name: "amount",
+        message: "please select amount",
+        type: "list",
+        choices: ["1000", "2000", "5000", "10000"],
+        when(resp) {
+          return resp.SelectedType == "fastcash";
+        },
+      },
+      {
+        name: "amount",
+        message: "please enter amount",
+        when(resp) {
+          return resp.SelectedType == "withdraw";
+        },
+      },
+    ]);
+    if (resp.SelectedType == "balanceinquiry") {
+      console.log(`avaialable balance is: ${user.balance}`);
+      const toRepeat = await inquirer.prompt([
+        {
+          name: "repeat",
+          type: "confirm",
+          message: "do you want to repeat transaction?",
+        },
+      ]);
+      if (toRepeat.repeat === true) continueTransaction = true;
+      else {
+        continueTransaction = false;
+      }
+    } else {
+      user.balance = user.balance - resp.amount;
+
+      console.log(`your new balance is:${user.balance}`);
+      continueTransaction = false;
+    }
   }
 }
