4c44ecc66988e8e714532ec003661aac4ac43848
Syeda Maria <uroos12345@gmail.com>
2023-03-29 13:58:18 +0500

    ATM first working version

--- index.js
+++ index.js
@@ -1,12 +1,50 @@
-#!/user/bin/node env
+#! /user/bin/env node
 import inquirer from "inquirer";
-inquirer
-    .prompt([
+let user = {
+    name: "maria",
+    pin: 123,
+    balance: 10000,
+};
+const resp = await inquirer.prompt([
     {
         message: "please enter pin",
         name: "pin",
+        type: "password",
     },
-])
-    .then((responnse) => {
-    console.log(responnse);
-});
+]);
+if (Number(resp.pin) !== user.pin) {
+    console.log("password invalid");
+}
+else {
+    const resp = await inquirer.prompt([
+        {
+            name: "SelectedType",
+            message: "please select option",
+            type: "list",
+            choices: ["withdraw", "fastcash", "balanceinquiry"],
+        },
+        {
+            name: "amount",
+            message: "please select amount",
+            type: "list",
+            choices: ["1000", "2000", "5000", "10000"],
+            when(resp) {
+                return resp.SelectedType == "fastcash";
+            },
+        },
+        {
+            name: "amount",
+            message: "please enter amount",
+            when(resp) {
+                return resp.SelectedType == "withdraw";
+            },
+        },
+    ]);
+    if (resp.SelectedType == "balanceinquiry") {
+        console.log(`avaialable balance is: ${user.balance}`);
+    }
+    else {
+        user.balance = user.balance - resp.amount;
+        console.log(`your new balance is:${user.balance}`);
+    }
+}
--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-#!/user/bin/node env
+#! /user/bin/env node
 import inquirer from "inquirer";
 
 // inquirer
@@ -12,10 +12,58 @@ import inquirer from "inquirer";
 //     console.log(responnse);
 //   });
 
+type Usertype = {
+  name: string;
+  pin: number;
+  balance: number;
+};
+let user: Usertype = {
+  name: "maria",
+  pin: 123,
+  balance: 10000,
+};
+
 const resp = await inquirer.prompt([
   {
     message: "please enter pin",
     name: "pin",
+    type: "password",
   },
 ]);
-console.log("response", resp);
+
+if (Number(resp.pin) !== user.pin) {
+  console.log("password invalid");
+} else {
+  const resp = await inquirer.prompt([
+    {
+      name: "SelectedType",
+      message: "please select option",
+      type: "list",
+      choices: ["withdraw", "fastcash", "balanceinquiry"],
+    },
+
+    {
+      name: "amount",
+      message: "please select amount",
+      type: "list",
+      choices: ["1000", "2000", "5000", "10000"],
+      when(resp) {
+        return resp.SelectedType == "fastcash";
+      },
+    },
+    {
+      name: "amount",
+      message: "please enter amount",
+      when(resp) {
+        return resp.SelectedType == "withdraw";
+      },
+    },
+  ]);
+  if (resp.SelectedType == "balanceinquiry") {
+    console.log(`avaialable balance is: ${user.balance}`);
+  } else {
+    user.balance = user.balance - resp.amount;
+
+    console.log(`your new balance is:${user.balance}`);
+  }
+}
